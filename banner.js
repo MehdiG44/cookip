101e292da12dceaa8e1c6bb12893002fdf1f7b63
MehdiG44 <mehdi.gatbache@doctolib.com>
2021-06-04 08:25:08 +0200

    Updates

--- banner.js
+++ banner.js
@@ -1,5 +1,6 @@
 (() => {
   const script = document.getElementById("cookie-consent-script");
+  const color = script.getAttribute("data-color");
   let activeCategory;
   const setActiveCategory = (newState) => (activeCategory = newState);
 
@@ -97,7 +98,7 @@
 
   const optionSelector = (name) => {
     const selectorContainer = document.createElement("div");
-    selectorContainer.style.cssText = "display:flex;align-self:flex-start;";
+    selectorContainer.style.cssText = "display:flex;";
 
     const checkButton = document.createElement("button");
     checkButton.textContent = "✓";
@@ -142,7 +143,7 @@
     }
 
     carret.style.cssText = carret.style.cssText + "transform: rotate(180deg);";
-    detail.style.cssText = detail.style.cssText + "max-height:110px;";
+    detail.style.cssText = detail.style.cssText + "max-height:150px;";
     setActiveCategory({ category: category, detail: detail, carret: carret });
   };
 
@@ -151,8 +152,9 @@
     "position:fixed;top:0;height;100%;width:100%;height:100%;background-color:rgba(0, 0, 0, 0.7);display:flex;align-items:flex-end";
 
   const banner = document.createElement("div");
-  banner.style.cssText =
-    "background-color:white;padding:24px;border-top-left-radius:8px;border-top-right-radius:8px";
+  const bannerPadding =
+    window.screen.width < 700 ? "padding:1em;" : "padding:2em 3em;";
+  banner.style.cssText = `${bannerPadding};background-color:white;border-top-left-radius:8px;border-top-right-radius:8px;max-height:100%;overflow:auto;`;
 
   const header = document.createElement("div");
   header.style.cssText =
@@ -164,32 +166,35 @@
 
   const technicalCookieDetail = document.createElement("div");
   technicalCookieDetail.style.cssText =
-    "flex-basis: 100%;font-weight:normal;margin-top:4px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
+    "flex-basis: 100%;font-weight:normal;margin-top:8px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
   technicalCookieDetail.textContent =
     "Ces cookies sont nécessaires au bon fonctionnement du site ainsi qu’à la conservation de votre consentement en matière de cookies. Ils sont requis et ne peuvent être désactivés.";
 
   const technicalCookieCarret = document.createElement("span");
   technicalCookieCarret.textContent = "^";
-  technicalCookieCarret.style.cssText =
-    "transform: rotate(90deg);font-size: 30px;font-family: monospace;color: #d97706;height:20px;line-height:30px;transition:transform 300ms linear 0s;";
+  technicalCookieCarret.style.cssText = `transform: rotate(90deg);font-size: 30px;font-family: monospace;color: ${color};height:20px;line-height:30px;transition:transform 300ms linear 0s;`;
 
   const technicalCookieTextContent = document.createElement("span");
   technicalCookieTextContent.textContent = "Cookies techniques";
   technicalCookieTextContent.style.cssText = "margin-left: 12px;";
 
+  const technicalCookieHeader = document.createElement("div");
+  technicalCookieHeader.style.cssText =
+    "display:flex;align-items:center;font-size:14px;";
+  technicalCookieHeader.append(
+    technicalCookieCarret,
+    technicalCookieTextContent
+  );
+
   const technicalCookieText = document.createElement("a");
   technicalCookieText.style.cssText =
-    "width:200px;font-size:14px;display:flex;align-items:center;cursor:pointer;";
-  technicalCookieText.append(technicalCookieCarret, technicalCookieTextContent);
+    "width:70%;display:flex;cursor:pointer;flex-direction:column;";
+  technicalCookieText.append(technicalCookieHeader, technicalCookieDetail);
 
   const technicalCookie = document.createElement("div");
   technicalCookie.style.cssText =
-    "margin:16px 0;display:flex;align-items:center;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
-  technicalCookie.append(
-    technicalCookieText,
-    optionSelector("technical"),
-    technicalCookieDetail
-  );
+    "margin:16px 0;display:flex;align-items:baseline;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
+  technicalCookie.append(technicalCookieText, optionSelector("technical"));
   technicalCookieText.onclick = () =>
     handleCategoryDetail(
       technicalCookie,
@@ -199,32 +204,35 @@
 
   const statisticCookieDetail = document.createElement("div");
   statisticCookieDetail.style.cssText =
-    "flex-basis: 100%;font-weight:normal;margin-top:4px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
+    "flex-basis: 100%;font-weight:normal;margin-top:8px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
   statisticCookieDetail.textContent =
     "Ces cookies nous permettent de comprendre comment vous naviguez sur notre site et d'en suivre la fréquentation. Vous nous aidez ainsi à mesurer la performance du site et à améliorer la qualité de nos services.";
 
   const statisticCookieCarret = document.createElement("span");
   statisticCookieCarret.textContent = "^";
-  statisticCookieCarret.style.cssText =
-    "transform: rotate(90deg);font-size: 30px;font-family: monospace;color: #d97706;height:20px;line-height:30px;transition:transform 300ms linear 0s;";
+  statisticCookieCarret.style.cssText = `transform: rotate(90deg);font-size: 30px;font-family: monospace;color: ${color};height:20px;line-height:30px;transition:transform 300ms linear 0s;`;
 
   const statisticCookieTextContent = document.createElement("span");
   statisticCookieTextContent.textContent = "Mesure d'audience";
   statisticCookieTextContent.style.cssText = "margin-left: 12px;";
 
+  const statisticCookieHeader = document.createElement("div");
+  statisticCookieHeader.style.cssText =
+    "display:flex;align-items:center;font-size:14px;";
+  statisticCookieHeader.append(
+    statisticCookieCarret,
+    statisticCookieTextContent
+  );
+
   const statisticCookieText = document.createElement("a");
   statisticCookieText.style.cssText =
-    "width:200px;font-size:14px;display: flex;align-items: center;cursor:pointer;";
-  statisticCookieText.append(statisticCookieCarret, statisticCookieTextContent);
+    "width:70%;display:flex;cursor:pointer;flex-direction:column;";
+  statisticCookieText.append(statisticCookieHeader, statisticCookieDetail);
 
   const statisticCookie = document.createElement("div");
   statisticCookie.style.cssText =
-    "margin:16px 0;display:flex;align-items:center;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
-  statisticCookie.append(
-    statisticCookieText,
-    optionSelector("statistics"),
-    statisticCookieDetail
-  );
+    "margin:16px 0;display:flex;align-items:baseline;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
+  statisticCookie.append(statisticCookieText, optionSelector("statistics"));
   statisticCookieText.onclick = () =>
     handleCategoryDetail(
       statisticCookie,
@@ -234,63 +242,66 @@
 
   const adCookieDetail = document.createElement("div");
   adCookieDetail.style.cssText =
-    "flex-basis: 100%;font-weight:normal;margin-top:4px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
+    "flex-basis: 100%;font-weight:normal;margin-top:8px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
   adCookieDetail.textContent =
     "Ces cookies permettent à nos partenaires de vous proposer des publicités plus pertinentes sur Internet. Ils permettent la collecte et le traitement de données d'utilisation du site afin de vous proposer des publicités sur des sites ou applications tierces. Ce choix n'impactera pas le nombre de publicités que vous verrez sur Internet.";
 
   const adCookieCarret = document.createElement("span");
   adCookieCarret.textContent = "^";
-  adCookieCarret.style.cssText =
-    "transform: rotate(90deg);font-size: 30px;font-family: monospace;color: #d97706;height:20px;line-height:30px;transition:transform 300ms linear 0s;";
+  adCookieCarret.style.cssText = `transform: rotate(90deg);font-size: 30px;font-family: monospace;color: ${color};height:20px;line-height:30px;transition:transform 300ms linear 0s;`;
 
   const adCookieTextContent = document.createElement("span");
   adCookieTextContent.textContent = "Publicité personnalisée";
   adCookieTextContent.style.cssText = "margin-left: 12px;";
 
+  const adCookieHeader = document.createElement("div");
+  adCookieHeader.style.cssText =
+    "display:flex;align-items:center;font-size:14px;";
+  adCookieHeader.append(adCookieCarret, adCookieTextContent);
+
   const adCookieText = document.createElement("a");
   adCookieText.style.cssText =
-    "width:200px;font-size:14px;display: flex;align-items: center;cursor:pointer;";
-  adCookieText.append(adCookieCarret, adCookieTextContent);
+    "width:70%;display:flex;cursor:pointer;flex-direction:column;";
+  adCookieText.append(adCookieHeader, adCookieDetail);
 
   const adCookie = document.createElement("div");
   adCookie.style.cssText =
-    "margin:16px 0;display:flex;align-items:center;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
-  adCookie.append(
-    adCookieText,
-    optionSelector("personnalization"),
-    adCookieDetail
-  );
+    "margin:16px 0;display:flex;align-items:baseline;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
+  adCookie.append(adCookieText, optionSelector("personnalization"));
   adCookieText.onclick = () =>
     handleCategoryDetail(adCookie, adCookieDetail, adCookieCarret);
 
   const adStatisticCookieDetail = document.createElement("div");
   adStatisticCookieDetail.style.cssText =
-    "flex-basis: 100%;font-weight:normal;margin-top:4px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
+    "flex-basis: 100%;font-weight:normal;margin-top:8px;overflow:hidden;max-height:0;transition:max-height 300ms ease-out 0s;";
   adStatisticCookieDetail.textContent =
     "Ces cookies permettent la collecte et le traitement de données d'utilisation du site afin de mesurer la performance de nos campagnes publicitaires et de partager ces données avec nos partenaires. Ce choix n'impactera pas le nombre de publicités que vous verrez sur Internet.";
 
   const adStatisticCarret = document.createElement("span");
   adStatisticCarret.textContent = "^";
-  adStatisticCarret.style.cssText =
-    "transform: rotate(90deg);font-size: 30px;font-family: monospace;color: #d97706;height:20px;line-height:30px;transition:transform 300ms linear 0s;";
+  adStatisticCarret.style.cssText = `transform: rotate(90deg);font-size: 30px;font-family: monospace;color: ${color};height:20px;line-height:30px;transition:transform 300ms linear 0s;`;
 
   const adStatisticTextContent = document.createElement("span");
   adStatisticTextContent.textContent = "Mesure de performance publicitaire";
   adStatisticTextContent.style.cssText = "margin-left: 12px;";
 
+  const adStatisticCookieHeader = document.createElement("div");
+  adStatisticCookieHeader.style.cssText =
+    "display:flex;align-items:center;font-size:14px;";
+  adStatisticCookieHeader.append(adStatisticCarret, adStatisticTextContent);
+
   const adStatisticCookieText = document.createElement("a");
   adStatisticCookieText.style.cssText =
-    "width:200px;font-size:14px;display: flex;align-items: center;cursor:pointer;";
-  adStatisticCookieText.append(adStatisticCarret, adStatisticTextContent);
+    "width:70%;display:flex;cursor:pointer;flex-direction:column;";
+  adStatisticCookieText.append(
+    adStatisticCookieHeader,
+    adStatisticCookieDetail
+  );
 
   const adStatisticCookie = document.createElement("div");
   adStatisticCookie.style.cssText =
-    "margin:16px 0;display:flex;align-items:center;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
-  adStatisticCookie.append(
-    adStatisticCookieText,
-    optionSelector("marketing"),
-    adStatisticCookieDetail
-  );
+    "margin:16px 0;display:flex;align-items:baseline;justify-content:space-around;font-weight:bold;flex-wrap:wrap;";
+  adStatisticCookie.append(adStatisticCookieText, optionSelector("marketing"));
   adStatisticCookieText.onclick = () =>
     handleCategoryDetail(
       adStatisticCookie,
@@ -308,8 +319,7 @@
 
   const optionsButton = document.createElement("button");
   optionsButton.textContent = "Options";
-  optionsButton.style.cssText =
-    "color:#d97706;font-weight:bold;font-size:14px;background:0;border:0;padding:0;line-height:unset;";
+  optionsButton.style.cssText = `color:${color};font-weight:bold;font-size:14px;background:0;border:0;padding:0;line-height:unset;`;
   optionsButton.onclick = () =>
     text.contains(optionsContainer) ? closeOptions() : openOptions();
 
@@ -322,13 +332,13 @@
   text.textContent = `${script.getAttribute(
     "data-company-name"
   )} et ses partenaires déposent des cookies et utilisent des informations non sensibles provenant de votre appareil pour améliorer ses produits, afficher des publicités et proposer des contenus personnalisés. Vous pouvez accepter ou refuser ces services. Vos choix sont conservés pendant 12 mois. Vous pouvez les modifier à tout moment en cliquant sur le lien dédié en bas de page. Pour en savoir plus sur les cookies, les données utilisées et leur traitement, vous pouvez consulter notre `;
-  text.style.cssText = "font-size:12px;margin:0 12px 12px 0;";
+  text.style.cssText = "font-size:13px;margin:0 12px 12px 0;";
 
   const link = document.createElement("a");
   link.textContent = "politique en matière de cookies.";
   link.href = script.getAttribute("data-privacy-policy-link");
   link.target = "_blank";
-  link.style.cssText = "color:#d97706;";
+  link.style.cssText = `color:${color};`;
 
   const buttonContainer = document.createElement("div");
   buttonContainer.style.cssText = "display:flex;";
@@ -336,8 +346,7 @@
   const refuseButtonContainer = document.createElement("div");
   const refuseButton = document.createElement("button");
   refuseButton.textContent = "Refuser";
-  refuseButton.style.cssText =
-    "width:max-content;min-width:8em;color:white;background-color:#d97706;border-width:1px;border-color:#d97706;border-radius:8px;margin:12px;padding:8px 18px;line-height:unset;";
+  refuseButton.style.cssText = `width:max-content;min-width:8em;color:white;background-color:${color};border-width:1px;border-color:${color};border-radius:8px;margin:12px;padding:8px 18px;line-height:unset;`;
   refuseButton.onclick = () =>
     setCookies({
       statistics: false,
@@ -348,16 +357,14 @@
   const acceptButtonContainer = document.createElement("div");
   const acceptButton = document.createElement("button");
   acceptButton.textContent = "Accepter & Fermer";
-  acceptButton.style.cssText =
-    "width:max-content;min-width:8em;color:white;background-color:#d97706;border-width:1px;border-color:#d97706;border-radius:8px;margin:12px;padding:8px 18px;line-height:unset;";
+  acceptButton.style.cssText = `width:max-content;min-width:8em;color:white;background-color:${color};border-width:1px;border-color:${color};border-radius:8px;margin:12px;padding:8px 18px;line-height:unset;`;
   acceptButton.onclick = () =>
     setCookies({ statistics: true, marketing: true, personnalization: true });
 
   const saveButtonContainer = document.createElement("div");
   const saveButton = document.createElement("button");
   saveButton.textContent = "Enregistrer";
-  saveButton.style.cssText =
-    "width:max-content;min-width:8em;color:white;background-color:#d97706;border-width:1px;border-color:#d97706;border-radius:8px;margin:12px;padding:8px 18px;opacity:0.3;line-height:unset;";
+  saveButton.style.cssText = `width:max-content;min-width:8em;color:white;background-color:${color};border-width:1px;border-color:${color};border-radius:8px;margin:12px;padding:8px 18px;opacity:0.3;line-height:unset;`;
   saveButton.disabled = true;
   saveButton.style.cursor = "default";
   saveButton.onclick = () => setCookies(cookieOptions);

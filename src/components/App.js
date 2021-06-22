c178e8e74eddbc3741dd5a77a47d5f29a6e91673
MehdiG44 <mehdi.gatbache@doctolib.com>
2021-06-22 08:49:25 +0200

    intro

--- src/components/App.js
+++ src/components/App.js
@@ -29,9 +29,16 @@ const App = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-6xl font-title font-bold w-11/12 text-center mt-8">
+      <h1 className="text-6xl font-title font-bold w-11/12 text-center my-8">
         Cookie banner generator
       </h1>
+      <div className="text-xl">
+        Free cookie consent banner script generator.
+      </div>
+      <div className="text-xl">
+        Can be used on its own or via Google Tag Manager.
+      </div>
+      <div className="text-xl">Available in English and French.</div>
       <Form />
     </div>
   );
